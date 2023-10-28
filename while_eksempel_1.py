255b5d978ad8ea0cebd1b731e55f05761d82dd04
OC1989 <142886551+OC1989@users.noreply.github.com>
2023-10-28 16:57:44 +0200

    Create while_eksempel_1.py

---
+++ while_eksempel_1.py
@@ -0,0 +1,12 @@
+# Dette er et eksempel på en while-løkke i Python
+
+# Sett en teller til 0
+teller = 0
+
+# Start en while-løkke som kjører så lenge telleren er mindre enn 5
+while teller < 5:
+    print("Telleren er nå:", teller)  # Skriv ut tellerverdien
+    teller = teller + 1  # Øk telleren med 1
+
+# Utskrift når løkken er ferdig
+print("Løkken er ferdig.")
