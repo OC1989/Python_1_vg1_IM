cece49fdaf515291d6a702dc51f3926783611201
OC1989 <142886551+OC1989@users.noreply.github.com>
2023-10-28 17:19:51 +0200

    Update for_eksempel_2.py

--- for_eksempel_2.py
+++ for_eksempel_2.py
@@ -1,8 +1,8 @@
 # Eksempel på en for-løkke som utfører en matematisk operasjon
 
 # Lag en liste med tall
-tall = [2, 4, 6, 8, 10]
+liste = [2, 4, 6, 8, 10]
 
 # Bruk en for-løkke til å doble hvert tall i listen og skrive det ut
-for element in tall:
+for element in liste:
     print(element * 2)
