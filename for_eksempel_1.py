78e624a35905014df0797835ce080bb256507ff9
OC1989 <142886551+OC1989@users.noreply.github.com>
2023-10-28 17:15:20 +0200

    Create for_eksempel_1.py

---
+++ for_eksempel_1.py
@@ -0,0 +1,15 @@
+# Eksempel på en for-løkke som itererer gjennom en liste
+
+# Lag en liste med navn på frukt
+frukt = ["eple", "banan", "appelsin", "jordbær", "drue"]
+
+# Bruk en for-løkke til å skrive ut hvert element i listen
+for element in frukt:
+    print("Jeg liker " + element)
+
+# Output vil være:
+# Jeg liker eple
+# Jeg liker banan
+# Jeg liker appelsin
+# Jeg liker jordbær
+# Jeg liker drue
