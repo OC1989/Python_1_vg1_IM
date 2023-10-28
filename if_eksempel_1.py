c80127360488c800e4d13981dcd581b578712a32
OC1989 <142886551+OC1989@users.noreply.github.com>
2023-10-28 16:40:41 +0200

    Create if_eksempel_1.py

---
+++ if_eksempel_1.py
@@ -0,0 +1,9 @@
+#Eksempel på if-setning - valg av morgenhilsen
+
+svar=input("Hei, har du en god morgen? Svar J eller N: ")
+if svar == "J":
+    print("Bra! Gi et smil, og du får 1000 tilbake")
+elif svar == "N":
+    print("Så leit, håper du snart blir bedre!")
+else:
+    print("Du svarte noe jeg ikke forstår, ønsker deg en god dag!")
