e56d51fc34c15b76a9516b3be6a85fa7376cc29f
OC1989 <142886551+OC1989@users.noreply.github.com>
2023-10-28 17:45:34 +0200

    Create funksjons_eksempel_1.py

---
+++ funksjons_eksempel_1.py
@@ -0,0 +1,13 @@
+# Importer modulen random i Python. Når du importerer random, gir det tilgang til funksjoner for å generere tilfeldige tall.
+import random
+
+# Definer en funksjon for å kaste terning
+def kast_terning():
+    resultat = random.randint(1, 6)
+    return resultat
+
+# For å kjøre en funksjon må man kalle funksjonen:
+kast = kast_terning()
+
+# Til slutt skriver vi ut resultatet med {kast} som mellomliggende variabel
+print(f'Du kastet {kast} på terningen')
