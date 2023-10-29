51fa36c8abfe2b58e1b18c4bb820ae55451090c0
OC1989 <142886551+OC1989@users.noreply.github.com>
2023-10-29 11:01:52 +0100

    Update funksjon_eksempel_1.py

--- funksjon_eksempel_1.py
+++ funksjon_eksempel_1.py
@@ -6,7 +6,7 @@ def lag_hilsen(navn):
 # Spør brukeren om navnet deres
 navn = input("Hva er navnet ditt? ")
 
-# Kall funksjonen for å lage hilsen, der vi også setter inn 'navn' som argument.
+# Kall funksjonen for å lage hilsen, der vi også setter inn 'navn' som argument, en måte å sende informasjon inn i funksjonen.
 hilsen = lag_hilsen(navn)
 
 # Vis hilsenen
